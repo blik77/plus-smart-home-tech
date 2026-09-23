6c5f24c50ce9efb438898353c82e1dea54ae0b2e
Dmitry Uglanov <dimarik.pay2009@yandex.ru>
2026-09-23 19:12:15 +0300

    fix issue 14

--- commerce/order-service/src/main/java/ru/yandex/practicum/order/client/ProductClient.java
+++ commerce/order-service/src/main/java/ru/yandex/practicum/order/client/ProductClient.java
@@ -10,15 +10,14 @@ import org.springframework.web.client.RestTemplate;
 @RequiredArgsConstructor
 public class ProductClient {
 
-    private static final String PRODUCT_URL_TEMPLATE = "http://product-service/api/products/%d";
-
     private final RestTemplate restTemplate;
 
     public ProductInfo getProduct(Long productId) {
 
-        log.info("Запрос информации о товаре из product-service: {}", PRODUCT_URL_TEMPLATE);
+        String url = "http://product-service/api/products/" + productId;
+        log.info("Запрос информации о товаре из product-service: {}", url);
         try {
-            return restTemplate.getForObject(PRODUCT_URL_TEMPLATE, ProductInfo.class);
+            return restTemplate.getForObject(url, ProductInfo.class);
         } catch (Exception e) {
             log.error("Не удалось получить товар {} из product-service", productId, e);
             return null;
